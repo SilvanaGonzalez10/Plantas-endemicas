a1e03644c564274a139c2bc5543ef35f9d1919b5
Cheems_de_traje <silvana17pgc@gmail.com>
2025-06-02 01:49:22 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,144 @@
+<!DOCTYPE html> 
+<html>
+        
+     <head>
+        <link rel="icon" href="icono2.png">
+        <title>Plantas Endemicas de Yucatan</title>
+            <style>
+        h1{
+            font-family: arial;
+            color: white;
+            background-image: url("banner.jpg");
+            background-size: 100%;
+            padding: 20px;
+        }
+
+        p{
+            font-size: 19;
+        }
+
+        h2{
+            font-size: 33;
+        }
+
+        ul{
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: rgb(214, 167, 177);
+         }
+
+         li{
+            float: left;
+         }
+
+         li a{
+            display: block;
+            color: rgb(231, 243, 253);
+            text-align: center;
+            padding: 15px 40px;
+            text-decoration: none;
+         }
+
+         li a:hover {
+            background-color:  rgb(211, 101, 125);
+         }
+    </style>
+    </head>
+
+     <Body background="namelesscv-20210208165913-1612838000.png">
+        <center>
+         <table width="80%" border="0" cellpadding="30px" bgcolor="#f9e0e3">
+            <tr>
+                <td>
+                    <center> <h1> <font size="9px">Plantas Endémicas de Yucatán </font> </h1> </center>
+                    <p></p>
+                    <center>  <section id="menu"> </section><hr width="90%"> </center>  
+                    <br>
+                    <h2>  5 Plantas Endémicas </h2>
+                    <p></p>
+                    <ul>
+                        <li> <a href="#balche">Balché</a> </li>
+                        <li> <a href="#chaka"> Chaká </a> </li>
+                        <li> <a href="#jabin"> Jabin </a> </li>
+                        <li> <a href="#pich"> Pich </a> </li>
+                        <li> <a href="#makulis"> Makulís Rosa</a> </li>
+                    </ul>
+
+                    <p> El estado de Yucatán alberga una rica diversidad de flora, muchas de ellas endémicas o nativas de la región. Estas plantas no solo forman parte del paisaje característico de la península, sino que también tienen un profundo valor cultural, medicinal y ecológico. A continuación, te presento una mirada general, enfocándonos en algunas especies clave:
+                    </p>
+                     <br></br>
+                <center> <hr width="90%"> </center>
+                    <p></p>
+                    <section id="balche">
+                    <h2> Balché </h2>
+                    </section>
+
+                    <p></p>
+                    <img src="balche.jpg" width="500px">
+                    <p>
+                        El balché (Lonchocarpus longistylus) es un árbol nativo de la Península de Yucatán, México, que se caracteriza por su follaje denso, flores lilas-moradas y frutos en forma de vainas planas. Es un árbol sagrado para los mayas, utilizado en ceremonias para obtener una bebida alcohólica llamada "balché" mediante fermentación de la corteza. 
+                        <a href="#menu"> <img src="regreso.png"> </a>
+                    </p>
+
+                <center> <hr width="90%"> </center>
+                    <p></p>
+                    <section id="chaka">
+                    <h2> Chaká </h2>
+                    </section>
+                    <p></p>
+                    <img src="chaka.jpg" width="500px">
+                    <p>
+                        Esta planta sirve como remedio para aliviar la calentura y se utiliza en cualquiera persona sin importar el sexo y la edad, las hojas de la chaca son utilizadas machacándoles con las manos y revolviéndolos con sal y limón , esto ese revuelve con otras plantas lla- mado malango, ya que sus hojas grandes y frescas. 
+                        <a href="#menu"> <img src="regreso.png"> </a>
+                     </p>
+
+                <center> <hr width="90%"> </center>
+                    <p></p>
+                    <section id="jabin">
+                    <h2> Jabin </h2>
+                    </section>
+                    <p></p>
+                    
+                    <img src="jabin.jpg" width="500px">
+                    <p>
+                        Los frutos del jabín, son vainas indehiscentes que van desde 1.5 hasta 8 cm de largo, posee cuatro alas membranosas con el margen ondulado, las vainas son de color amarillentas y pubescentes, con el cáliz y el estilo persistentes, pueden llegar a contener desde 1 hasta diez semillas reniformes de 5 x 3 mm.
+                        <a href="#menu"> <img src="regreso.png"> </a>
+                    </p>
+
+                <center> <hr width="90%"> </center>
+                    <p></p>
+                    <section id="pich">
+                    <h2> Pich </h2>
+                    </section>
+                    <p></p>
+                    
+                    <img src="pich.jpg" width="500px">
+                    <p>
+                        Árbol de 20 a 30 m (hasta 45 m) de altura, con un diámetro de hasta 3 m. Las hojas son brevidecidua, los árboles pierden sus hojas cuando fructifican. La floración se realiza de febrero a junio. La polinización es entomófila, palomillas y abejorros en cuanto a los frutos inicia en febrero, madurando de abril a julio.
+                        <a href="#menu"> <img src="regreso.png"> </a>
+                    </p>
+
+                <center> <hr width="90%"> </center>
+                    <p></p>
+                    <h2> <section id="makulis"> Makulís Rosa </h2>
+                    </section>
+                    <p></p>
+                    <section id="balche">
+                    <img src="makulis.jpg" width="450px">
+                    <p>
+                        Árbol de hasta 15 mts. de altura, el tronco es recto y fisurado y la copa piramidal; las flores son de color rosa-morado, muy vistosas, dispuestas al final de las ramas; los frutos son cápsulas de 30 a 40 cms. de largo, ligeramente retorcidos.
+                        <a href="#menu"> <img src="regreso.png"> </a>
+                    </p>
+
+                </td>
+            </tr>
+         </table>
+         
+    <footer> <Center> <h4> <font color="white"> Derechos Reservados gonzales-y-quijano©copyj2025 </font> </h4> </Center> </footer> 
+
+        </center> 
+    </body>
+
+</html>
